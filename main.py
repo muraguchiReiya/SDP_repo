cacec471f4ccad405acc065a3a81f9a8d159fdb6
muraguchiReiya <b2202360@photon.chitose.ac.jp>
2023-05-11 01:56:08 +0900

    画像出力設定

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import psycopg2
 from dotenv import load_dotenv
 import os
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from matplotlib import ticker
 
 load_dotenv()
 
@@ -15,10 +18,30 @@ dsn = f"dbname={db} host={host} user={user} password={pswd}"
 
 app = Flask(__name__)
 
+def create_image(df):
+    time=df['time'].dt.strftime('%Y-%m-%d-%H:%M')
+    sns.set()
+    fig, ax = plt.subplots(figsize=(15,5))
+    ax.plot(time, df['temp'])
+    ax.set(xlabel='time', ylabel='temp')
+    ax.legend()
+    ax.xaxis.set_major_locator(ticker.MultipleLocator(10))
+    labels = ax.get_xticklabels()
+    plt.setp(labels
+             #, rotation=90
+     )
+    plt.savefig('./static/image/temp.png', bbox_inches='tight', pad_inches=0)
+
+    plt.clf()
+    fig, ax = plt.subplots(figsize=(15, 10))
+    sns.distplot(df['temp'], kde=False, rug=False)
+    plt.savefig('./static/image/hist.png', bbox_inches='tight', pad_inches=0)
+
+
 def create_dict(df):
     value=[]
     for i in range(len(df)):
-        data={'time':df.iat[i,0],'temp':df.iat[i,1]}
+        data={'time':df.iat[i,2],'temp':df.iat[i,1]}
         value.append(data)
     value=list(reversed(value))
     return value
@@ -28,9 +51,10 @@ def route():
     sql= 'SELECT * FROM temp;'
     conn = psycopg2.connect(dsn)
     df = pd.read_sql(sql=sql, con=conn)
-    df['time']=pd.to_datetime(df['time']).dt.strftime('%Y年%m月%d日%H時%M分')
+    df['time']=pd.to_datetime(df['time'])
+    df['view_time']=df['time'].dt.strftime('%Y年%m月%d日%H時%M分')
     conn.close()
-
+    create_image(df)
     data=create_dict(df)
 
     return render_template('SDP_demo.html',data=data)
